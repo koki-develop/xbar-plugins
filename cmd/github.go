80817d4aa353310cbbb128f516edb1b518f8da16
koki-develop <kou.pg.0131@gmail.com>
2024-01-14 10:27:16 +0900

    Add functionality to search and display user's pull requests

--- cmd/github.go
+++ cmd/github.go
@@ -21,24 +21,43 @@ var githubCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-
-		repos, err := c.SearchPullRequestsReviewRequested()
-		if err != nil {
-			return err
-		}
-
 		cnt := 0
-		for _, repo := range repos {
-			cnt += len(repo.PullRequests)
-		}
-		fmt.Fprintf(v, "GitHub (%d)\n", cnt)
-		fmt.Fprintln(v, "---")
 
-		fmt.Fprintln(v, "Review Requested")
-		for _, repo := range repos {
-			fmt.Fprintf(v, "%s/%s | size=12\n", repo.Owner, repo.Name)
-			for _, pr := range repo.PullRequests {
-				fmt.Fprintf(v, "%s | href=%s\n", pr.Title, pr.URL)
+		{
+			repos, err := c.SearchPullRequestsReviewRequested()
+			if err != nil {
+				return err
+			}
+
+			for _, repo := range repos {
+				cnt += len(repo.PullRequests)
+			}
+			fmt.Fprintf(v, "GitHub (%d)\n", cnt)
+			fmt.Fprintln(v, "---")
+
+			fmt.Fprintln(v, "Review Requested")
+			for _, repo := range repos {
+				fmt.Fprintf(v, "%s/%s | size=12\n", repo.Owner, repo.Name)
+				for _, pr := range repo.PullRequests {
+					fmt.Fprintf(v, "%s | href=%s\n", pr.Title, pr.URL)
+				}
+			}
+		}
+
+		{
+			fmt.Fprintln(v, "---")
+			fmt.Fprintln(v, "My Pull Requests")
+
+			repos, err := c.SearchPullRequestsMine()
+			if err != nil {
+				return err
+			}
+
+			for _, repo := range repos {
+				fmt.Fprintf(v, "%s/%s | size=12\n", repo.Owner, repo.Name)
+				for _, pr := range repo.PullRequests {
+					fmt.Fprintf(v, "%s | href=%s\n", pr.Title, pr.URL)
+				}
 			}
 		}
 
--- internal/github/pr.go
+++ internal/github/pr.go
@@ -44,16 +44,67 @@ func (c *Client) SearchPullRequestsReviewRequested() ([]*Repository, error) {
 		return nil, err
 	}
 
-	reposmap := map[*Repository][]*PullRequest{}
+	prs := []*PullRequest{}
 	for _, edge := range q.Search.Edges {
-		pr := &PullRequest{
+		prs = append(prs, &PullRequest{
 			Title: edge.Node.PullRequest.Title,
 			URL:   edge.Node.PullRequest.URL,
 			Repository: &Repository{
 				Name:  edge.Node.PullRequest.Repository.Name,
 				Owner: edge.Node.PullRequest.Repository.Owner.Login,
 			},
-		}
+		})
+	}
+
+	return c.groupByRepository(prs), nil
+}
+
+func (c *Client) SearchPullRequestsMine() ([]*Repository, error) {
+	var q struct {
+		Search struct {
+			Edges []struct {
+				Node struct {
+					PullRequest struct {
+						Title      string
+						URL        string
+						Repository struct {
+							Name  string
+							Owner struct {
+								Login string
+							}
+						}
+					} `graphql:"... on PullRequest"`
+				}
+			}
+		} `graphql:"search(query: $query, type: ISSUE, first: 100)"`
+	}
+	filter := strings.Join([]string{
+		"is:pr",
+		"is:open",
+		"author:@me",
+	}, " ")
+	if err := c.gql.Query("query", &q, map[string]any{"query": graphql.String(filter)}); err != nil {
+		return nil, err
+	}
+
+	prs := []*PullRequest{}
+	for _, edge := range q.Search.Edges {
+		prs = append(prs, &PullRequest{
+			Title: edge.Node.PullRequest.Title,
+			URL:   edge.Node.PullRequest.URL,
+			Repository: &Repository{
+				Name:  edge.Node.PullRequest.Repository.Name,
+				Owner: edge.Node.PullRequest.Repository.Owner.Login,
+			},
+		})
+	}
+
+	return c.groupByRepository(prs), nil
+}
+
+func (c *Client) groupByRepository(prs []*PullRequest) []*Repository {
+	reposmap := map[*Repository][]*PullRequest{}
+	for _, pr := range prs {
 		reposmap[pr.Repository] = append(reposmap[pr.Repository], pr)
 	}
 
@@ -63,5 +114,5 @@ func (c *Client) SearchPullRequestsReviewRequested() ([]*Repository, error) {
 		repos = append(repos, repo)
 	}
 
-	return repos, nil
+	return repos
 }
