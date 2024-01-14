82448234670838fc40eefae4ed5bf71147330245
koki-develop <kou.pg.0131@gmail.com>
2024-01-14 10:19:41 +0900

    Add github command to search pull requests

---
+++ cmd/github.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/koki-develop/xbar-plugins/internal/github"
+	"github.com/spf13/cobra"
+)
+
+var (
+	githubToken string
+)
+
+var githubCmd = &cobra.Command{
+	Use: "github",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		v := new(strings.Builder)
+
+		c, err := github.NewClient(githubToken)
+		if err != nil {
+			return err
+		}
+
+		repos, err := c.SearchPullRequestsReviewRequested()
+		if err != nil {
+			return err
+		}
+
+		cnt := 0
+		for _, repo := range repos {
+			cnt += len(repo.PullRequests)
+		}
+		fmt.Fprintf(v, "GitHub (%d)\n", cnt)
+		fmt.Fprintln(v, "---")
+
+		fmt.Fprintln(v, "Review Requested")
+		for _, repo := range repos {
+			fmt.Fprintf(v, "%s/%s | size=12\n", repo.Owner, repo.Name)
+			for _, pr := range repo.PullRequests {
+				fmt.Fprintf(v, "%s | href=%s\n", pr.Title, pr.URL)
+			}
+		}
+
+		fmt.Println(v.String())
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(githubCmd)
+}
